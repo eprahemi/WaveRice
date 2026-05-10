c0664247b8b296cc4dd1fad22f1d173d1883751c
eprahemi <eprahemi@users.noreply.github.com>
2026-05-11 01:43:08 +0300

    Bump to v1.7.1 for update notification

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 #  One-liner: bash -c "$(curl -fsSL https://raw.githubusercontent.com/eprahemi/WifeRice/main/install.sh)"
 # ===========================================================================
 
-DOTS_VERSION="1.7.0"
+DOTS_VERSION="1.7.1"
 DOTS_VERSION_NAME=""
 
 set -e
