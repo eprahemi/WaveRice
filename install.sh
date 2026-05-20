5eb709e6a8a8f93dcd17ff38aa46fa206be3a3db
eprahemi <eprahemi@github.com>
2026-05-20 19:22:10 +0300

    v1.7.56: fix Guide popup changelog stuck at v1.7.52

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 #  One-liner: bash -c 'eval "$(curl -fsSL https://raw.githubusercontent.com/eprahemi/WifeRice/main/install.sh)"'
 # ===========================================================================
 
-DOTS_VERSION="1.7.55"
+DOTS_VERSION="1.7.56"
 DOTS_VERSION_NAME=""
 
 set -e
